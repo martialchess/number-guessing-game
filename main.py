866d3718dac80f0c4614fadaada8f2d3980e9be6
Rida Malik <internalprogression@gmail.com>
2025-09-03 08:13:37 +0500

    Added Welcome messages

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from art import logo
 
 def play_game():
     print(logo)
+    print("Welcome to the number guessing game!")
+    print("I'm thinking of a number between 1 and 100")
 
     # Set difficulty
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
